4e585f7f4e1637debbf2d2e379d33268aac4b9b8
Mohammad H. Lotfazar <lotfazardev@users.noreply.github.com>
2020-08-10 18:29:37 +0430

    Updates

---
+++ precache-manifest.38e07a2992c60fd6303c80bc67817b62.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6246734b51731185fdfec1987170df70",
+    "url": "/soally/index.html"
+  },
+  {
+    "revision": "3c552c68e71289d7808d",
+    "url": "/soally/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "62dd1e9414b74d3f458e",
+    "url": "/soally/static/css/main.82f0b068.chunk.css"
+  },
+  {
+    "revision": "3c552c68e71289d7808d",
+    "url": "/soally/static/js/2.257ffe38.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/soally/static/js/2.257ffe38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62dd1e9414b74d3f458e",
+    "url": "/soally/static/js/main.b91c0e48.chunk.js"
+  },
+  {
+    "revision": "bf8a8576cac626ad1505",
+    "url": "/soally/static/js/runtime-main.4d975b8f.js"
+  },
+  {
+    "revision": "64cadbed17904aa2569e44604df8fa5c",
+    "url": "/soally/static/media/Yekan-Bakh.64cadbed.TTF"
+  }
+]);
